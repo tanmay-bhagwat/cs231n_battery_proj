b22528b60c7acfee64bea16f089ea3913a37b380
tanmay-bhagwat <131923532+tanmay-bhagwat@users.noreply.github.com>
2023-05-14 09:58:19 -0700

    Update CNN_main.py
    - Added code to include current 'i' (mA) column in DataFrame
    - Updated data loading code to use the 'data_loader' function

--- CNN_main.py
+++ CNN_main.py
@@ -11,29 +11,45 @@ from utils import *
 os.chdir(r'/Users/tanmay/Desktop/CS231N/Project/')
 
 
-# # Converting MATLAB data to DataFrame
+# # Working with ExampleDC_C1.mat 
+# fin_dict=data_loader()
+# #Convert dictionary to DataFrame
+# df=pd.DataFrame.from_dict(fin_dict)
+# df.head()
 
-#Convert MATLAB data to dictionary
-mat_contents=loadmat('ExampleDC_C1.mat')
-data=mat_contents['ExampleDC_C1']['ch'][0][0]
-data_cp=data.copy()
-dict_vals=[x[:,0] for x in data[0][0]]
-dict_keys=list(data.dtype.names)
-fin_dict=dict(zip(dict_keys, dict_vals))
+# # Dropping all columns not v,T or i
+# df=df.drop(['t','q'],axis=1)
+# df.head()
 
-#Convert dictionary to DataFrame
-df=pd.DataFrame.from_dict(fin_dict)
-df.head()
-
-# Dropping all columns not v,T or i
-df=df.drop(['t','q'],axis=1)
-df.head()
+# slices = slicer(df)
+# images = []
+# for i in range(len(slices)):
+#     image=Image.fromarray(make_im(slices[i]),mode='RGB')
+#     #image.show()
+#     images.append(image)
+# #images[0].show()
 
 
+# Working with full Oxford dataset
+fin_dict = data_loader(mode=1)
+df = pd.DataFrame.from_dict(fin_dict['Cell2']['cyc0100'])
+
+# Making a current 'i' column, such that i = d(q)*3600/dt, where dt = 1
+j = 0
+df['i'] = [0]*len(df)
+for j in range(1, len(df)):
+  df['i'][j] = (df['q'][j] - df['q'][j-1])*3600    
+print(df.head(), type(df['i']))                    
+
+# Slicing given dataframe into chunks
 slices = slicer(df)
+#print(fin_dict['Cell2']['cyc0100'].shape)
+#print(len(slices))
+
+# Converting chunks into images
 images = []
+i = 0
 for i in range(len(slices)):
-    image=Image.fromarray(make_im(slices[i]),mode='RGB')
-    #image.show()
+    image = Image.fromarray(make_im(slices[i]),mode='RGB')
     images.append(image)
-images[0].show()
+#print(len(images))
