9781488a49e16708b8312d8942b42630646f640b
tanmay-bhagwat <131923532+tanmay-bhagwat@users.noreply.github.com>
2023-05-16 19:50:08 -0700

    Update CNN_main.py
    - Added code to use make_im_Image function instead of _make_im
    - Updated code for calculating current column in DataFrame, was getting SettingWithCopy warnings

--- CNN_main.py
+++ CNN_main.py
@@ -28,12 +28,16 @@ os.chdir(r'/Users/tanmay/Desktop/CS231N/Project/')
 fin_dict = data_loader(mode=1)
 df = pd.DataFrame.from_dict(fin_dict['Cell2']['cyc0100'])
 
+###################################################################
+
+#CURRENT COLUMN ADDITION
+
 # Making a current 'i' column, such that i = d(q)*3600/dt, where dt = 1
 j = 0
 df['i'] = [0]*len(df)
 for j in range(1, len(df)):
-  df['i'][j] = (df['q'][j] - df['q'][j-1])*3600    
-print(df.head(), type(df['i']))                    
+  df.loc[df.index[j],'i'] = (df.loc[df.index[j],'q'] - df.loc[df.index[j-1],'q'])*3600
+  
 ###################################################################
 
 #PREPROCESSING STEPS
@@ -41,20 +45,23 @@ print(df.head(), type(df['i']))
 # Slicing given dataframe into chunks
 slices = slicer(df)
 
-# Converting chunks into images using matplotlib
-images = []
-i = 0
-for i in range(len(slices)):
-  image = make_im(slices[i]) #image is a (15,15,3) np array
-  images.append(image)
+# Converting chunks into images using matplotlib, not used since several functions are internally using PIL directly or indirectly
+# Conversion from np array to Image object causes some changes in pixel values
+# images = []
+# i = 0
+# for i in range(len(slices)):
+#   image = make_im(slices[i]) #image is a (15,15,3) np array
+#   images.append(image)
 #plt.imshow(images[0])
 #plt.show()
 
-# Converting chunks into images using PIL
-# i = 0
-# for i in range(len(slices)):
-#     image = Image.merge("RGB", make_im(slices[i])) #image is an Image object
-#     images.append(image)
-#     #image.show()
+i = 0
+images = []
+for i in range(len(slices)):
+    image = Image.merge("RGB", make_im_Image(slices[i]))
+    images.append(image)
+    #image.show()
+#images[0].show()
+#print(im)
 
 ###################################################################
