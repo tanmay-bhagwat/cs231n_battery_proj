e04e467e6a6b6d12565718f5f07be43960d21b07
tanmay-bhagwat <131923532+tanmay-bhagwat@users.noreply.github.com>
2023-05-19 14:11:03 -0700

    Update utils.py
    - Updated structure of file; using torch class Dataset to build the battery dataset for use in DataLoader
    - Added dataset_visualize function for sampling a few images from the dataset
    - Uses the loader function, which uses DataPreprocessor class for constructing the dataset from json files

--- utils.py
+++ utils.py
@@ -1,142 +1,98 @@
 import numpy as np
 import pandas as pd
-import math
+import torch
+from torchvision.io import read_image
+import os
+from Data_Preprocessor import *
+import matplotlib.pyplot as plt
+import torchvision.transforms as T
 
-# # Introducing overlap between image data
 
-# How do we decide how much overlap to use in two adjacent subintervals?
-# Considering the fact that we allow for overlap between adjacent chunks
-# Overlap is a hyperparam here, selected mainly to be able to use all the given datapoints even when len(df) % 225 != 0
-# 
-# We select overlap such that we try to distribute the excess overlap between last and second last panels among all other panels in the full interval and try to equalize overlap between adjacent panels
-# 
-# ex. Consider the case where L=840, n=225. Typically, we would get only 840//225=3 panels from the whole data
-# However, we would like to use the full data as much as possible. Hence, we would like to use the remaining 165 rows as well. This means there will be an overlap of 60 between image 3 and new image 4
-# We would like to distribute this excess overlap equally among all other images in the data, such that the overlap between two adjacent images is nearly equal
-# 
-# Now, we get the overlap by moving the start location of each image to the left, overlapping with the end of the previous image. i.e. (Start of image 2) < 225 (end of image 1). (If there was no overlap, the end of image i would be immediately to the left of start of image i+1).
-# 
-# So, we want our start of the last image to be at L-n (so as to use full data)
-# We currently start at n*/floor(L/n). 
-# Hence, excess overlap = n*/floor(L/n) - (L-n)
-# We have ceil(L/n) total images (panels), and we can only move ceil(L/n)-2 panels (since start and end panel positions are fixed)
-# For ceil(L/n)-2 panels, we have ceil(L/n)-1 overlaps to use!
-# Hence, adjacent overlap = (n*/floor(L/n) - (L-n))/(ceil(L/n)-1)
+# Refer to this: https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+# Also this: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html#dataset-class
 
-################### Explanation code ###################
-# import math
-# n=225
-# L=len(df)
-# print(L)
-
-# init_M=math.floor(L/n)
-# print(init_M)
-# overlap=math.floor((init_M*n-(L-n))/(math.ceil(L/n)-1)) 
-# print(overlap)
-
-# #Using this overlap value, we can now fit in the last set of data to get a new panel, so that total=ceil(L/n)
-
-# indices = np.array([n*j-j*overlap for j in range(0,math.ceil(L/n))])
-# indices = np.where(indices>0,indices,0)
-# print(indices)
-######################################################
-
-def slicer(df: pd.DataFrame) -> pd.DataFrame:
+class BatteryDataset(torch.utils.data.Dataset):
     """
-    Function takes in DataFrame and returns slices as per overlap explanation above
-    Still have to force the last index to be L-n (unclear how to get the correct last index from overlap formula)
-    Inputs:
-    df : pd.DataFrame
+    Derived class from the abstract dataset class from torch. 
+    Retrieves samples as Image object (image) and capacity (label), using annotations file
+    """
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.image_dataset = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_dataset)
     
-    Return:
-    slices: Slice view of pd.DataFrame
-    """
-    L = len(df)
-    n = 225
-    init_M = math.floor(L/n)
-    overlap = math.floor((init_M*n-(L-n))/(math.ceil(L/n)-1)) 
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.to_list()
+
+        # Because of the way the annotations file was constructed, the first column are just the row numbers.
+        # Hence, image filenames are column 2, labels are column 1. 
+        img_path = os.path.join(self.root_dir, self.image_dataset.iloc[idx, 2])
+        image = read_image(img_path)
+        label = self.image_dataset.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+            
+        return {'image':image, 'label': label}
     
-    indices = np.array([n*j-j*overlap for j in range(0,math.ceil(L/n))])
-    indices = np.where(indices>0,indices,0)
-    indices[-1] = L-n
-    slices = [df.iloc[j:j+n,:] for j in indices]
     
-    return slices
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C x H x W
+        image = image.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image),
+                'label': label}
 
 
-def make_im_Image(df_slice: pd.DataFrame) -> np.ndarray:
+def loader(test_cell) -> None:
     """
-    Function returns a normalized (15,15,3) array from given slice of DataFrame
-    Order of stacking is 'i','v','T'
-    Inputs:
-    df_slice: Slice of DataFrame, containing keys 'i','v','T', each of size 225
-    
-    Return:
-    plot_arr: np.ndarray
+    Function to call Data_Preprocessor object. Uses DataFrames to write the csv annotations files.
+
+    Parameters:
+    test_cell (int): The cell which is the test data
     """
-    im_arr=[]
-    for key in ['i','v','T']:
-        arr = np.array(df_slice[key])
-        plot_arr = 255*(arr-np.min(arr))/(np.max(arr)-np.min(arr))
-        im_arr.append(Image.fromarray(plot_arr.reshape((15,15))).convert('L'))
-        
-    return im_arr
+    # Selected cell (test_cell) as the test cell
+    # df is then the training dataset DataFrame, can write it to a csv file and use in PyTorch Dataset class
+    index_list = [i for i in range(1,9) if i != test_cell]
 
+    obj = Data_Preprocessor('/Users/tanmay/Desktop/CS231N/Project/Code_files/')
+    dataset_dict = obj.data_processor()
 
-def _make_im(df_slice: pd.DataFrame) -> np.ndarray:
-    """
-    Function returns a normalized (15,15,3) array from given slice of DataFrame
-    Order of stacking is 'i','v','T'
-    Inputs:
-    df_slice: Slice of DataFrame, containing keys 'i','v','T', each of size 225
-    
-    Return:
-    plot_arr: np.ndarray
-    """
-    im_arr=[]
-    for key in ['i','v','T']:
-        arr = np.array(df_slice[key])
-        plot_arr = (arr-np.min(arr))/(np.max(arr)-np.min(arr))
-        im_arr.append(plot_arr.reshape((15,15)))
-    plot_arr = np.dstack(im_arr)
-        
-    return plot_arr
+    # Constructing the training and test annotation files
+    df = pd.DataFrame()
+    for k in index_list:
+        df = pd.concat((df,pd.DataFrame.from_dict(dataset_dict[k])))
 
+    df_test = pd.DataFrame.from_dict(dataset_dict[test_cell])
 
-def data_loader(mode=0):
-    """
-    Function loads data from required dataset based on 'mode' keyword. 
-    Inputs:
-    mode: int, 0 for ExampleDC_C1.mat or 1 for Oxford full dataset
-    
-    Return:
-    If mode = 0, then returns a dict with headings 'i', 'v', 't', 'T', 'q'
-    If mode = 1, then returns a dict of dicts, where each outer key is the cell #, inner dict is the cycle number 
-    with DataFrame of 'q', 'v', 'T', 't'
-    """
-    if mode == 0:
-        # Working "ExampleDC_C1.mat" MATLAB data to DataFrame
-        # Converts all DC_C1.ch data from struct into dict
-        mat_contents = loadmat('ExampleDC_C1.mat')
-        data = mat_contents['ExampleDC_C1']['ch'][0][0]
-        dict_vals = [x[:,0] for x in data[0][0]]
-        dict_keys = list(data.dtype.names)
-        fin_dict = dict(zip(dict_keys, dict_vals))
+    # df.to_csv('./image_dir/Training_data.csv')
+    # df_test.to_csv('./image_dir/Test_data.csv')
 
-        return fin_dict 
-    
-    if mode == 1:
-        # Working with Oxford_Battery_Degradation_Dataset_1.mat
-        # Converts all of the Cell{i}.ch data from struct to dict
-        mat_contents = loadmat("Oxford_Battery_Degradation_Dataset_1.mat")
-        data_dict = {f"Cell{i}":{} for i in range(1,9)}
-        for i in range(1,9):
-            for cycle in mat_contents[f'Cell{i}'].dtype.names:
-                data = mat_contents[f'Cell{i}'][cycle][0][0][0][0][0][0][0]
-                dict_vals = [x[:,0] for x in data]
-                dict_keys = list(data.dtype.names)
-                fin_dict = dict(zip(dict_keys, dict_vals))
-                data_dict[f'Cell{i}'][cycle] = pd.DataFrame.from_dict(fin_dict)
-        
-        return data_dict
+def dataset_visualize(batt_dataset) -> None:
+    """Display images from the dataset."""
 
+    for i in range(len(batt_dataset)):
+        sample = batt_dataset[i]
+
+        # print(i, sample['image'].shape, sample['label'].shape)
+        label = sample['label']
+
+        ax = plt.subplot(1, 4, i + 1)
+        plt.tight_layout()
+        ax.set_title(f'Sample #{i})')
+        ax.set_xlabel(f'{label:0.5f}')
+        # ax.axis('off')
+        plt.imshow(np.array(T.ToPILImage()(sample['image'])))
+
+        if i == 3:
+            plt.show()
+            break
