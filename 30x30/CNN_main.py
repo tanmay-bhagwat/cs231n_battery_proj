efd48fc066648733b8038725d2536e45f35f4f3f
tanmay-bhagwat <131923532+tanmay-bhagwat@users.noreply.github.com>
2023-06-02 20:18:27 -0700

    Update CNN_main.py
    - Removed dead code and updated modules imported
    - Added model saving and loading functions

--- 30x30/CNN_main.py
+++ 30x30/CNN_main.py
@@ -4,18 +4,19 @@ import numpy as np
 import os
 from utils import *
 from BatteryDataset import *
-from Net_model import *
+from models import * 
 import torch.optim as optim
 from torch.utils.data import DataLoader, sampler, Subset
 
-# cwd to directory containing the data as json files
+# cwd to directory containing the json_data directory
+# This directory will be considered the parent directory for the json_data, image_dir_C1, image_dir_OCV directories later
 os.chdir(r'/Users/tanmay/Desktop/CS231N/Project/Code_files/')
 
 # Generating the dataset
 # Select the test cell, mode, pass to build_dataset()
 mode = "C1"
 test_cell = 1 
-build_dataset(test_cell, path=".", slice_len=900, mode=mode)
+# build_dataset(test_cell, path=".", slice_len=900, mode=mode)
 
 batt_dataset = BatteryDataset(csv_file=f'./image_dir_{mode}/Training_data.csv', root_dir=f'./image_dir_{mode}')
 test_dataset = BatteryDataset(csv_file=f'./image_dir_{mode}/Test_data.csv', root_dir=f'./image_dir_{mode}')
@@ -23,8 +24,6 @@ test_dataset = BatteryDataset(csv_file=f'./image_dir_{mode}/Test_data.csv', root
 # To visualize a few samples from the dataset
 # dataset_visualize(batt_dataset, mode="OCV")
 
-# num_workers < number of cores on computer
-# batch_size in multiples of 8, since internally PyTorch stores data as 8-bit integers
 # Paper divided dataset in a 70:30 split, batch_size=128, for epochs=80
 NUM_TRAIN = int(len(batt_dataset)*0.7)
 
@@ -35,19 +34,17 @@ validation_loader = DataLoader(batt_dataset, batch_size=128, sampler = sampler.S
 test_loader = DataLoader(batt_dataset, batch_size=128, num_workers=4)
 
 # Enumerate and print the batches that each of the loaders has
-# if __name__ == '__main__':
-#     for i_batch, sample_batched in enumerate(train_loader):
-#         print(i_batch, sample_batched[0].size(), sample_batched[1].size())
-#         if sample_batched[0].dtype != torch.float32:
-#             raise ValueError('Incorrect dtypes')
-#         # print(type(sample_batched[0]))
+for i_batch, sample_batched in enumerate(train_loader):
+    print(i_batch, sample_batched[0].size(), sample_batched[1].size())
+    if sample_batched[0].dtype != torch.float32:
+        raise ValueError('Incorrect dtypes')
 
-#     # Define a model and the optimizer
-#     model = Net()
-#     # L2 regularization via weight_decay: https://pytorch.org/docs/stable/generated/torch.optim.Adam.html
-#     optimizer = optim.Adam(model.parameters(), lr=3e-3, weight_decay=1e-5)
-#     # train_model(model, optimizer, train_loader, val_loader=validation_loader, epochs=20)
-#     # torch.save(model.state_dict(), 'Net30.pth')
+# Define a model and the optimizer
+model = SqueezeNet().to(device)
+# L2 regularization via weight_decay: https://pytorch.org/docs/stable/generated/torch.optim.Adam.html
+optimizer = optim.Adam(model.parameters(), lr=3e-3, weight_decay=1e-5)
+train_model(model, optimizer, train_loader, val_loader=validation_loader, epochs=20)
+torch.save(model.state_dict(), 'SqueezeNet30_DC.pth')
 
 #     loader = DataLoader(test_dataset, batch_size=32, num_workers=4)
 #     model = Net()
